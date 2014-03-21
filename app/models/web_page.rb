43b9c1be6e2a791065e8fb681afd0fcf71ce9df4
Róbert Viðar Bjarnason <robert@citizens.is>
2014-03-21 15:24:15 +0000

    Cleanup

--- app/models/web_page.rb
+++ app/models/web_page.rb
@@ -22,29 +22,4 @@ class WebPage < ActiveRecord::Base
     store_classification_entity("concepts",AlchemyAPI::ConceptTagging.new.search(:url => self.url))
     self.save
   end
-
-  def classify_test
-    test = [
-        {
-            "text" => "er ssd", "relevance" => "0.993164"
-        },
-        {
-            "text" => "ccccccccccc", "relevance" => "0.913164"
-        },
-        {
-            "text" => "bbbbbbbbbbbb", "relevance" => "0.523164"
-        },
-        {
-            "text" => "xxxxxxxxxxxxx", "relevance" => "0.323164"
-        },
-        {
-            "text" => "yyyyyyyyyyyyyyy", "relevance" => "0.223164"
-        },
-    ]
-    store_classification_entity("entities", test)
-    store_classification_entity("keywords", test)
-    store_classification_entity("concepts", test)
-    puts self.inspect
-  end
-
 end
