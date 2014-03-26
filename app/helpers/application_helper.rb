6dd6779fb8f159102421e6b80a39f2d80a9b0c45
Robert Bjarnason <robert@citizens.is>
2014-03-26 00:51:49 +0000

    Only add ones with screenshots

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def create_carousel_urls_from_pages(pages)
     urls = ""
     pages.each_with_index do |page,i|
-      urls += "{url:'#{page.screenshot.url(:full)}',width:1366,height:768},"
+      urls += "{url:'#{page.screenshot.url(:full)}',width:1366,height:768}," if page.screenshot? and page.screenshot.exists?
     end
     urls
   end
