a1c4449057bf15b91f627b118ec63eb4d24297bd
Róbert Viðar Bjarnason <robert@citizens.is>
2014-04-01 17:48:38 +0000

    Tweaks to search

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -36,24 +36,21 @@ class ExperimentsController < ApplicationController
         (@page.concepts_med_relevance ? ",#{@page.concepts_med_relevance}," : ",")+
         (@page.concepts_low_relevance ? @page.concepts_low_relevance : "")
 
-
     #@all_search_items = @all_search_items.gsub("/"," ").split(",").reject{|w| w==""}
     @all_entities = @all_entities.gsub("/"," ").gsub("&"," ").gsub("-"," ").split(",").reject{|w| w==""}.reject{|x| x.empty? || x.split(" ").count>3 || SKIP.include?(x)}.map{|w| " (#{w}) "}
     @all_keywords = @all_keywords.gsub("/"," ").gsub("&"," ").gsub("-"," ").split(",").reject{|w| w==""}.reject{|x| x.empty? || x.split(" ").count>3 || SKIP.include?(x)}.map{|w| " (#{w}) "}
     @all_concepts = @all_concepts.gsub("/"," ").gsub("&"," ").gsub("-"," ").split(",").reject{|w| w==""}.reject{|x| x.empty? || x.split(" ").count>3 || SKIP.include?(x)}.map{|w| " (#{w}) "}
 
     @all_title_words = @page.title.split(" ").map{|x| x.gsub("!","").gsub("'","").gsub("-"," ").gsub("?","")}.reject{|x| x.empty? || x.split(" ").count>2 || SKIP.include?(x) || x.length<3}
-    @all_title_words_grouped = []
-    @all_title_words.each_slice(2).each do |words|
-      @all_title_words_grouped << words.map{|w| "(#{w})"}.join(" ")
-    end
 
     Rails.logger.debug @all_keywords
     @all_search_items = @all_entities[0..1]+@all_entities+
                         @all_keywords+
                         @all_concepts+
+                        @all_title_words+@all_title_words+@all_title_words+@all_title_words+@all_title_words+@all_title_words+@all_title_words+@all_title_words+
                         @all_title_words+@all_title_words+@all_title_words+@all_title_words+@all_title_words+@all_title_words+@all_title_words+@all_title_words
-    Rails.logger.debug "--------------------------------------------------------------"
+
+        Rails.logger.debug "--------------------------------------------------------------"
     Rails.logger.debug @all_search_items
   end
 
@@ -66,14 +63,15 @@ class ExperimentsController < ApplicationController
   end
 
   def search
-    news_hits = ThinkingSphinx.search(@query, :ranker=>:wordcount,  :star=>true, :@field_weights=>@field_weights, :with => {:web_page_type_id => WebPageType.where(:name=>"news").first.id})[0..5]
-    industry_hits = ThinkingSphinx.search(@query, :ranker=>:wordcount, :star=>true, :@field_weights=>@field_weights, :with => {:web_page_type_id => WebPageType.where(:name=>"industry").first.id})[0..1]
+    news_hits = ThinkingSphinx.search(@query, :ranker=>:wordcount, :star=>true, :@field_weights=>@field_weights, :with => {:web_page_type_id => WebPageType.where(:name=>"news").first.id})[0..6]
+    industry_hits = ThinkingSphinx.search(@query, :star=>true, :@field_weights=>@field_weights, :with => {:web_page_type_id => WebPageType.where(:name=>"industry").first.id})[0..1]
     #@excerpter = ThinkingSphinx::Excerpter.new 'web_page_core', @query
+    #@hits = (news_hits+news_title_hits+industry_hits).shuffle
     @hits = (news_hits+industry_hits).shuffle
     Rails.logger.debug @hits.inspect
     #@hits += ThinkingSphinx.search(@page.concepts_high_relevance)[0..2]
     @hits = @hits.reject{|p| p.id==@page.id || p.url.include?("nhs-citizen")}.uniq
-    @hits = @hits[0..8]
+    @hits = @hits[0..9]
     #@hits.context.panes << ThinkingSphinx::Panes::ExcerptsPane
   end
 
--- app/indices/web_page_index.rb
+++ app/indices/web_page_index.rb
@@ -2,13 +2,13 @@ ThinkingSphinx::Index.define :web_page, :with => :active_record do
   # fields
   indexes title
   indexes entities_high_relevance
-  indexes entities_med_relevance
+  #indexes entities_med_relevance
   #indexes entities_low_relevance
   indexes keywords_high_relevance
-  indexes keywords_med_relevance
+  #indexes keywords_med_relevance
   #indexes keywords_low_relevance
   indexes concepts_high_relevance
-  indexes concepts_med_relevance
+  #indexes concepts_med_relevance
   #indexes concepts_low_relevance
 
   # attributes
