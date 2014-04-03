3f98a2667219a0113bc67724d8116bbc5708f49d
Róbert Viðar Bjarnason <robert@citizens.is>
2014-04-03 19:31:51 +0000

    Force http for now because of iframe news loading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter do
+    if request.ssl? && Rails.env.production?
+      redirect_to :protocol => 'http://', :status => :moved_permanently
+    end
+  end
+
 end
